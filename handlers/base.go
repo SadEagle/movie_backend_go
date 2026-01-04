49e92cff21faff6a4e48d61c681f1b6b44bb1f17
Sandy Solomon <sadsandeagle@gmail.com>
2026-01-04 21:33:38 +0500

    Simplify response body writing

--- handlers/base.go
+++ handlers/base.go
@@ -8,14 +8,8 @@ import (
 )
 
 func writeResponseBody[T any](rw http.ResponseWriter, responseByteObj T, responseObjName string) {
-	responseFavMovieByte, err := json.Marshal(responseByteObj)
-	if err != nil {
-		log.Println(err)
-		http.Error(rw, fmt.Sprintf("Can't convert %s data to response json", responseObjName), 500)
-		return
-	}
-
-	_, err = rw.Write(responseFavMovieByte)
+	var enc = json.NewEncoder(rw)
+	err := enc.Encode(responseByteObj)
 	if err != nil {
 		log.Println(err)
 		http.Error(rw, fmt.Sprintf("Can't send %s data", responseObjName), 500)
